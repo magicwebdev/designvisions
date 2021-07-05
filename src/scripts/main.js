cf3e9ee180901f287fa314e59628625eafe43835
Александра Храпкова <axi83@mail.ru>
2021-07-05 15:59:37 +0300

    finish section reviews

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,17 @@ $(function(){
     dots: true,
     arrows: false,
     autoplay: true,
-      // slidesToShow: 5,
-      // slidesToScroll: 1,
-
+  });
+  $('.reviews__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button class="slick-arrow slick-arrow--prev"><img src="assets/images/icons/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-arrow--next"><img src="assets/images/icons/arrow-right.svg" alt="arrow-right"></button>'
   });
 });
