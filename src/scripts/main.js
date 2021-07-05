248c7113bb812ef4df3c3f994445dcc0b505dbe0
Александра Храпкова <axi83@mail.ru>
2021-07-05 08:12:37 +0300

    finish section portfolio

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('.portfolio__slider').slick({
+    centerMode: true,
+    centerPadding: '210px',
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+      // slidesToShow: 5,
+      // slidesToScroll: 1,
+
+  });
+});
