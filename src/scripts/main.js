32f7bd61d6f86b51aa79b48f6a29113df31694c7
Александра Храпкова <axi83@mail.ru>
2021-07-06 10:37:00 +0300

    finish adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
-$(function(){
+$(function () {
   $('.portfolio__slider').slick({
     centerMode: true,
     centerPadding: '210px',
-    slidesToShow: 1,
+
     dots: true,
     arrows: false,
     autoplay: true,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 1160,
+        settings: {
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          centerMode: false,
+          dots: false,
+        },
+      },
+    ],
   });
   $('.reviews__slider').slick({
     slidesToShow: 1,
@@ -16,7 +32,16 @@ $(function(){
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    prevArrow: '<button class="slick-arrow slick-arrow--prev"><img src="assets/images/icons/arrow-left.svg" alt="arrow-left"></button>',
-    nextArrow: '<button class="slick-arrow slick-arrow--next"><img src="assets/images/icons/arrow-right.svg" alt="arrow-right"></button>'
+    prevArrow:
+      '<button class="slick-arrow slick-arrow--prev"><img src="assets/images/icons/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-arrow--next"><img src="assets/images/icons/arrow-right.svg" alt="arrow-right"></button>',
+  });
+  $('.header__toggle-btn').on('click', function () {
+    $('.header__toggle-btn--1').toggleClass('active');
+    $('.header__toggle-btn--2').toggleClass('active');
+    $('.header__toggle-btn--3').toggleClass('active');
+    $('.header__menu').toggleClass('active');
+    $('.header__menu-btn').toggleClass('active');
   });
 });
