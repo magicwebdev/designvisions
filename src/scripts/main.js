d954649add5e6b49b6b07d776dec11da3cfe6d7b
Александра Храпкова <axi83@mail.ru>
2021-07-06 17:45:38 +0300

    finish project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -44,4 +44,69 @@ $(function () {
     $('.header__menu').toggleClass('active');
     $('.header__menu-btn').toggleClass('active');
   });
+  //E-mail Ajax Send
+	$('.contact__form').on('submit', function() {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function() {
+			setTimeout(function() {
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
+
+window.addEventListener('load', function () {
+  let menu = document.querySelector('.header__menu-list');
+  let links = menu.querySelectorAll('.header__menu-link');
+  let btnUp = document.querySelector('.up__btn');
+  let scrollTimeout;
+
+  window.addEventListener('scroll', function () {
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(actionsOnScroll, 200);
+  });
+
+  function actionsOnScroll() {
+    let scrollPosition = window.pageYOffset;
+    if (scrollPosition > window.innerHeight) {
+      btnUp.classList.add('up__btn-visible');
+    } else {
+      btnUp.classList.remove('up__btn-visible');
+    }
+  }
+
+  menu.addEventListener('click', function (e) {
+    let link = e.target;
+    if (link.classList.contains('header__menu-link')) {
+      e.preventDefault();
+      scrollToAnchor(link.hash);
+    }
+  });
+
+  if (location.hash !== '') {
+    scrollToAnchor(location.hash);
+  }
+
+  btnUp.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+
+  function scrollToAnchor(id) {
+    let anchor = document.querySelector(id);
+    if (anchor !== null) {
+      let position = anchor.offsetTop; // если необходмо отступить от верха, вычесть нужное число (anchor.offsetTop - 70)
+      window.scrollTo({
+        top: position,
+        behavior: 'smooth',
+      });
+    }
+  }
 });
